440cb2ea7c56db8e3296bb7298092457774ad37c
Lorena Garcia <lorena_65_11@hotmail.com>
2022-08-22 14:57:36 +0200

    Sessions and Log out

--- project2/models/user.model.js
+++ project2/models/user.model.js
@@ -3,12 +3,17 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    username: {
+      type: String,
+      trim: true,
+      required: [true, 'Username is required.'],
+      unique: true
+    },
     email: {
       type: String,
       trim: true,
       required: [true, 'Email is required.'],
       unique: true
-      // unique: true -> Ideally, should be unique, but its up to you
     },
     passwordHash:{ 
       type: String,
