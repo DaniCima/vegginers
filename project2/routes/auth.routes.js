1fa1f1e537fcb73d2f94714753b9c857a078b616
Lorena Garcia <lorena_65_11@hotmail.com>
2022-08-22 11:36:49 +0200

    sign up page

--- project2/routes/auth.routes.js
+++ project2/routes/auth.routes.js
@@ -1,8 +1,32 @@
 const router = require("express").Router();
 const User = require('../models/User.model');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 router.get("/signup", (req, res) => {
     res.render("auth.views/signup");
   });
+  router.get("/signup", (req, res) => {
+    res.render("auth/signup");
+  });
+
+  router.post("/signup", (req, res) => {
+    const { email, password } = req.body;
+   
+    bcrypt
+      .genSalt(saltRounds)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hashedPassword => {
+        return User.create({
+          email,
+          passwordHash: hashedPassword
+        });
+      })
+      .then(userFromDB => {
+         console.log('Newly created user is: ', userFromDB);
+        res.redirect('/auth/profile');
+      })
+      .catch(error => console.log(error)); 
+  })
 
   module.exports = router;
